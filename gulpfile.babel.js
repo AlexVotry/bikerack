b72fb6928f46cd8928473ebb9b000868a5fac3a7
petejh <petejh-200.59@q.com>
2016-08-16 12:23:15 -0700

    Add test framework for unit testing
    Install npm packages:
    * Mocha (via gulp-mocha): Javascript test framework
    * Chai: BDD assertion library providing the 'expect' syntax
    * SuperTest: abstraction library for testing HTTP services
    * SuperTest as Promised: Promise interface for SuperTest
    
    Provide new Gulp tasks to run unit tests:
    * `gulp mocha` collects and passes all specs to the mocha test runner
    * `gulp test` aliases the 'mocha' task
    The 'test' task is further aliased in package.json to enable `npm test`.
    
    A placeholder spec (spec/dummy.spec.js) is provided to verify that mocha
    is operational and the new gulp tasks are working. This file should be
    wholly replaced by meaningful, functional specs as development
    continues.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,16 @@
 'use strict';
 
 import gulp from 'gulp';
+import mocha from 'gulp-mocha';
 
 gulp.task('default', () => {
   console.log('<gulp>... It works!');
+});
+
+gulp.task('test', ['mocha']);
+
+gulp.task('mocha', () => {
+  gulp.src('spec/**/*spec.js', { read: false })
+    .pipe(mocha({ reporter: 'spec' }));
 });
 
---
+++ spec/dummy.spec.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+
+describe('mocha test runner', () => {
+
+  // passing test
+  it('knows the truth when it sees it', () => {
+    expect(true).to.be.true;
+  });
+
+  // failing test
+  it('knows the truth when it does not see it', () => {
+    expect(false).to.be.true;
+  });
+});
+

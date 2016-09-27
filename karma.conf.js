95408aa71b39b36bb1aa64edd230b41912297b48
petejh <petejh-200.59@q.com>
2016-09-27 05:22:07 +0000

    Add Sinon.JS library for test spies, stubs, and mocks

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha', 'chai', 'sinon-chai'],
 
 
     // list of files / patterns to load in the browser
@@ -67,3 +67,4 @@ module.exports = function(config) {
     concurrency: Infinity
   })
 }
+

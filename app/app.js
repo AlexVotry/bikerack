ceac8ca2e50ac0f9c39a3b43286e8a98247d21b6
petejh <petejh-200.59@q.com>
2016-11-03 23:49:17 +0000

    Respond with a token on POST /api/v1/signup
    The /api/v1/signup endpoint is used to register a user in the system.
    
    The server expects a POST request containing credentials for a user in
    JSON format within the request body.
    
    It returns a JSON web token (JWT) in the reponse body per IETF RFC 7519,
    with user identification data encoded in the payload.
    
    As implemented here, the server simply reads the username from the
    request and inserts that directly into a user object for the reponse; no
    user-registry is kept. At present, both the JWT header and signature
    fields are dummy strings.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import bodyParser from 'body-parser';
 import express from 'express';
 import path from 'path';
 
@@ -7,8 +8,25 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
+app.use(bodyParser.json());
+
 app.get('/', function(request, response) {
   response.sendFile('index.html');
+});
+
+app.post('/api/v1/signup', function(request, response) {
+  let username = request.body.username;
+
+  let user = { user: { name: username, id: 2 } };
+
+  let payload = JSON.stringify(user);
+  let encodedPayload = Buffer(payload).toString('base64');
+
+  let token = ['header', encodedPayload, 'signature'].join('.');
+
+  response
+    .status(201)
+    .json({ token: token });
 });
 
 module.exports = app;
--- app/app.spec.js
+++ app/app.spec.js
@@ -1,13 +1,45 @@
-var request = require('supertest-as-promised');
 var app = require('../app/app');
+var request = require('supertest-as-promised');
 
 describe('module app', function() {
+
   describe('GET /', function() {
     it('responds with status 200', function() {
       return request(app)
         .get('/')
         .expect(200);
     });
+
+    it('responds with html content', function() {
+      return request(app)
+        .get('/')
+        .expect('Content-Type', /html/);
+    });
   });
+
+  describe('POST /api/v1/signup', function() {
+    it('responds with status 201', function() {
+      return request(app)
+        .post('/api/v1/signup')
+        .expect(201);
+    });
+
+    it('responds with json content', function() {
+      return request(app)
+        .post('/api/v1/signup')
+        .expect('Content-Type', /json/);
+    });
+
+    it('responds with a token', function() {
+      const JWT_REGEX = /^\{\"token\":\"[\w\-\+\~\/]+\=*\.[\w\-\+\~\/]+\=*\.[\w\-\+\~\/]+\=*\"\}$/;
+
+      return request(app)
+        .post('/api/v1/signup')
+        .expect(JWT_REGEX);
+    });
+
+    // responds with status 400/401 for a malformed request, i.e. missing credentials
+  });
+
 });
 
